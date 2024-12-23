68498f81db660ac349db759f0b538b5d084b2bde
Pedro Igor <peuigor09@gmail.com>
2024-12-23 10:26:26 -0300

    Implementação da lógica de folha de pagamento microsserviço, implementado dados mockados

---
+++ src/main/java/com/example/hr_payroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.example.hr_payroll.services;
+
+import com.example.hr_payroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
